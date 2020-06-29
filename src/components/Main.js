445c65266c7f4680c279243c7950a264e7c797ce
ElladanTasartir <erickmalta100@gmail.com>
2020-06-29 12:51:14 -0300

    Funcionalidades adicionadas a ToDoList

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 
-import { FaPlus } from 'react-icons/fa';
+import { FaPlus, FaEdit, FaWindowClose } from 'react-icons/fa';
 
 import './Main.css';
 
 export default class Main extends Component {
   state = {
     novaTarefa: '',
+    tarefas: [],
+    index: -1,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        // Criando uma nova tarefa
+        tarefas: [...novasTarefas, novaTarefa],
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      });
+    }
+
+    this.setState({
+      novaTarefa: '', // para limpar o input que recebe esse state como valor
+    });
   };
 
   handleChange = (e) => {
@@ -15,18 +46,58 @@ export default class Main extends Component {
     });
   };
 
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+
+    const novasTarefas = [...tarefas];
+    // para não manipular a referência, já que o state
+    // não permite isso e sim, só substituição de referência
+
+    novasTarefas.splice(index, 1); // Vai pegar o indice e remover 1 elemento
+
+    this.setState({
+      tarefas: [...novasTarefas],
+    });
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
 
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
-        <form className="form" action="#">
+        <form onSubmit={this.handleSubmit} className="form" action="#">
           <input type="text" onChange={this.handleChange} value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
+        <ul className="tarefas">
+          {tarefas.map((tarefa, index) => (
+            <li key={tarefa}>
+              {tarefa}
+              <span>
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="delete"
+                />
+              </span>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
