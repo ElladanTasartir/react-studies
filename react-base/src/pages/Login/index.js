ca25e66ad8ef26c2bf642e78e8aa6a6c0c33fc2f
Erick Malta <erickmalta100@gmail.com>
2020-06-29 22:44:46 -0300

    Header component and Login in progress

---
+++ react-base/src/pages/Login/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <Container>
+      {/* Pode ser escrito como React.Fragment, é a mesma coisa */}
+      <Title isRed>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+      <button type="button">Enviar</button>
+    </Container>
+  );
+}
---
+++ react-base/src/styles/GlobalStyles.js
@@ -0,0 +1,52 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { primaryColor, primaryDarkColor } from '../config/colors';
+
+export default createGlobalStyle`
+ * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box ;
+ }
+
+ body {
+    font-family: sans-serif;
+    background: ${primaryDarkColor};
+    color: ${primaryDarkColor};
+ }
+
+ html, body, #root {
+     height: 100%;
+ }
+
+ button {
+    cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding: 10px 20px;
+    font-weight: 700;
+    border-radius: 4px;
+ }
+
+ a {
+    text-decoration: none;
+    background: ${primaryColor};
+ }
+
+ ul {
+    list-style: none;
+ }
+`;
+// Todo estilo colocado aqui dentro quando importado globalmente
+// afetará toda a aplicação
+
+export const Container = styled.section`
+  max-width: 360px;
+  background: #fff;
+  margin: 30px auto;
+  padding: 30px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
