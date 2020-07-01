b5b6e600c03e6236b1954da1f0d2531b6e3d9dc3
Erick Malta <erickmalta100@gmail.com>
2020-06-30 22:28:07 -0300

    Added a axios config to make requests to the API

--- react-base/src/pages/Login/index.js
+++ react-base/src/pages/Login/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
+import axios from '../../services/axios';
+
 export default function Login() {
+  useEffect(() => {
+    async function GetData() {
+      try {
+        const response = await axios.get('/alunos');
+        console.log(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    GetData();
+  }, []); // Quando vazio atualiza só na primeira montagem
+  // Array de dependências
+
   return (
     <Container>
       {/* Pode ser escrito como React.Fragment, é a mesma coisa */}
