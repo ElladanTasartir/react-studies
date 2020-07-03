def2a58ad58598523525da45cb2b693d7e6590e4
Erick Malta <erickmalta100@gmail.com>
2020-07-03 20:04:51 -0300

    :ok_hand: authorization redux stuff

---
+++ consumirapi/src/store/modules/auth/actions.js
@@ -0,0 +1,23 @@
+import * as types from '../types';
+
+// Desacoplar as actions e os types é uma boa prática na hora de se trabalhar com Redux
+export function loginRequest(payload) {
+  return {
+    type: types.LOGIN_REQUEST,
+    payload,
+  };
+}
+
+export function loginSuccess(payload) {
+  return {
+    type: types.LOGIN_SUCCESS,
+    payload,
+  };
+}
+
+export function loginFailure(payload) {
+  return {
+    type: types.LOGIN_FAILURE,
+    payload,
+  };
+}
---
+++ consumirapi/src/store/modules/auth/sagas.js
@@ -0,0 +1,40 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { get } from 'lodash';
+import * as actions from './actions';
+import * as types from '../types';
+import axios from '../../../services/axios';
+import history from '../../../services/history';
+
+function* loginRequest({ payload }) {
+  try {
+    // Aqui pedimos para o sagas realizar essa chamada no axios, passando ele como callback
+    // e os argumentos dele como args do call, ou seja, primeiro o callback e depois os argumentos dele
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.success('Você fez login');
+
+    // Colocamos o token no cabeçalho da requisição, no caso, de todas
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
+  } catch (err) {
+    toast.error('Usuário ou senha inválidos');
+
+    yield put(actions.loginFailure());
+  }
+}
+// Já que não usamos o yield nessa, não precisamos que esta seja uma função geradora
+function persistRehydrate({ payload }) {
+  const token = get(payload, 'auth.token', '');
+
+  if (!token) return;
+
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
